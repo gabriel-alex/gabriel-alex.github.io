3826dd09148a446cbc6a68d23edbf8c40b93ef62
GABRIEL Alex <alex.gabriel@univ-lorraine.fr>
2022-02-10 23:14:27 +0100

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/gabriel-alex.github.io/' : '/'
+  /* publicPath:
+    process.env.NODE_ENV === 'production' ? '/gabriel-alex.github.io/' : '/' */
+    publicPath: '/'
 }
